748739ddbc0afca8f0e580777679f7f6f86e0f98
Luiz3401 <zimmermanncastro@gmail.com>
2020-01-30 21:44:53 -0300

    Aula sobre strings

---
+++ aula3.js
@@ -0,0 +1,39 @@
+function add()
+{
+
+    var texto = document.getElementById("texto");
+    var cidade = texto.value;
+
+    var lista = document.getElementById("lista");
+
+    var existe = lista.innerHTML.indexOf(cidade);
+
+    if(existe >= 0)
+    {
+        window.alert("A cidade já foi cadastrada");
+    } else
+    {
+        lista.innerHTML += "<li>" + cidade + "</li>";
+    }
+
+    //limpa o input    
+    texto.value = "";
+   
+}
+function nome()
+{
+    var nome = document.getElementById("nome");
+    var box = document.getElementById("box");
+
+    box.innerHTML= nome.value.toUpperCase();
+}
+function letra()
+{
+    var nome = document.getElementById("nome");
+    var box = document.getElementById("box");
+
+    var letra = nome.charAt(0);
+    var resto = nome.value.Slice(1);
+
+    box.innerHTML= letra.toUpperCase() + resto.tolowerCase();
+}
